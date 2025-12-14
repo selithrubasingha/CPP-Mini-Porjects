ec70f1a738b07fe0ce27d3be4398d8e38ecf23f2
Selith Rubasingha <selithrubasingha@gmail.com>
2025-12-14 10:08:44 +0530

    limited scrolling to the right

--- building_my_own_text_editor/kilo.c
+++ building_my_own_text_editor/kilo.c
@@ -307,6 +307,8 @@ void editorRefreshScreen() {
 /*** input ***/
 
 void editorMoveCursor(int key) {
+  //used to access the row->size .. to access it , we initialize row here
+  erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
   switch (key) {
     case ARROW_LEFT:
       if (E.cx != 0) {
@@ -314,8 +316,9 @@ void editorMoveCursor(int key) {
       }
       break;
     case ARROW_RIGHT:
-
+      if (row && E.cx < row->size) {
         E.cx++;
+      }
         break;
     case ARROW_UP:
       if (E.cy != 0) {
