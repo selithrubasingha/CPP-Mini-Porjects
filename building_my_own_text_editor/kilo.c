b0524aca20cd5e1934a825608184fef844dcaecb
Selith <selithrubasingha@gmail.com>
2025-12-21 12:33:22 +0530

    added message bar and commented upto line 499

--- building_my_own_text_editor/kilo.c
+++ building_my_own_text_editor/kilo.c
@@ -8,6 +8,8 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <stdarg.h>
 #include <string.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
@@ -56,6 +58,8 @@ struct editorConfig
   int screencols;
   struct termios orig_termios;
   int numrows;
+  char statusmsg[80];
+  time_t statusmsg_time;
   erow *row;
   char* filename;
 };
@@ -336,16 +340,20 @@ void editorOpen(char *filename)
     die("fopen");
 
   char *line = NULL;
-  size_t linecap = 0;
-  ssize_t linelen;
-  // linelen = getline(&line, &linecap , fp);
+  size_t linecap = 0; //unsigned integer -> size_t (only positive values)
+  ssize_t linelen; //signed integer -> ssize
+  //when we call getline with a NULL pointer it will allocate memory for us ! default autopilot stuff
   while ((linelen = getline(&line, &linecap, fp)) != -1)
   {
+    //inside this while loop we remove the \n and \r at the end of each line (actually the linelen is the one that s decreased)
     while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
       linelen--;
 
+
+  //now we append the line to the editor rows
     editorAppendRow(line, linelen);
   }
+  //freeing the memory and closing the file
   free(line);
   fclose(fp);
 }
@@ -354,27 +362,32 @@ void editorOpen(char *filename)
 
 struct abuf
 {
-  char *b;
-  int len;
+  char *b;   // A pointer to the actual data in memory
+  int len;   // The current length of the string stored in 'b'
 };
 
 #define ABUF_INIT {NULL, 0}
 
 void abAppend(struct abuf *ab, const char *s, int len)
 {
+  //dynamically add stuff to the string buffer
   char *new = realloc(ab->b, ab->len + len);
 
+  //error handling
   if (new == NULL)
     return;
 
+  //this memcpy is used often better to remember it 
   memcpy(&new[ab->len], s, len);
+
+  //we ifrst made the allocation and here... we assign the pointers !
   ab->b = new;
-  ;
   ab->len += len;
 }
 
 void abFree(struct abuf *ab)
 {
+  //free the memory allocated for the buffer
   free(ab->b);
 }
 
@@ -390,6 +403,8 @@ void editorScroll()
   {
     E.rowoff = E.cy;
   }
+  // when the cy is larger than the screen itself (means the user has scrolled down)
+  //then there is a offset and we caculate it .
   if (E.cy >= E.rowoff + E.screenrows)
   {
     E.rowoff = E.cy - E.screenrows + 1;
@@ -408,13 +423,17 @@ void editorScroll()
 void editorDrawRows(struct abuf *ab)
 {
   int y;
+  //we draw only fixed amount of lines
   for (y = 0; y < E.screenrows; y++)
   {
+    //but we should start with rowoffset in mind!
     int filerow = y + E.rowoff;
     if (filerow >= E.numrows)
     {
+      //the below if statement happens if a file is not open
       if (E.numrows == 0 && y == E.screenrows / 3)
       {
+        //a buffer to store the welcome string 
         char welcome[80];
         int welcomelen = snprintf(welcome, sizeof(welcome),
                                   "Kilo editor -- version %s", KILO_VERSION);
@@ -437,16 +456,20 @@ void editorDrawRows(struct abuf *ab)
         abAppend(ab, "~", 1);
       }
     }
-    else
+    else //else means if a file is open
     {
+      //notice the file row contians the offset , and here we get the file line size 
       int len = E.row[filerow].rsize - E.coloff;
       if (len < 0)
         len = 0;
       if (len > E.screencols)
         len = E.screencols;
+      //render is a string ... so when we say render[x] it means start the line of string from x position! 
+      // that is how we add the column offset baby!
       abAppend(ab, &E.row[filerow].render[E.coloff], len);
     }
-
+    
+    //clear the rest of the line and go to the next line
     abAppend(ab, "\x1b[K", 3);
       abAppend(ab, "\r\n", 2);
     }
@@ -454,18 +477,28 @@ void editorDrawRows(struct abuf *ab)
 }
 
 void editorDrawStatusBar(struct abuf *ab) {
+  //escape sequence -> invert colors!
   abAppend(ab, "\x1b[7m", 4);
   
-  char status[80];
+  char status[80], rstatus[80];
   int len = snprintf(status, sizeof(status), "%.20s - %d lines",
     E.filename ? E.filename : "[No Name]", E.numrows);
+  int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d",
+    E.cy + 1, E.numrows);
   if (len > E.screencols) len = E.screencols;
   abAppend(ab, status, len);
   while (len < E.screencols) {
-    abAppend(ab, " ", 1);
-    len++;
+    if (E.screencols - len == rlen) {
+      abAppend(ab, rstatus, rlen);
+      break;
+    } else {
+      abAppend(ab, " ", 1);
+      len++;
+    }
   }
+  //escape sequence to stop inverting colors
   abAppend(ab, "\x1b[m", 3);
+  abAppend(ab, "\r\n", 2);
 }
 
 void editorRefreshScreen()
@@ -479,6 +512,7 @@ void editorRefreshScreen()
   abAppend(&ab, "\x1b[H", 3);
   editorDrawRows(&ab);
   editorDrawStatusBar(&ab);
+  editorDrawMessageBar(&ab);
 
   char buf[32];
   snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.rx - E.coloff) + 1);
@@ -490,6 +524,21 @@ void editorRefreshScreen()
   abFree(&ab);
 }
 
+void editorSetStatusMessage(const char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vsnprintf(E.statusmsg, sizeof(E.statusmsg), fmt, ap);
+  va_end(ap);
+  E.statusmsg_time = time(NULL);
+}
+
+void editorDrawMessageBar(struct abuf *ab) {
+  abAppend(ab, "\x1b[K", 3);
+  int msglen = strlen(E.statusmsg);
+  if (msglen > E.screencols) msglen = E.screencols;
+  if (msglen && time(NULL) - E.statusmsg_time < 5)
+    abAppend(ab, E.statusmsg, msglen);
+}
 /*** input ***/
 
 void editorMoveCursor(int key)
@@ -593,6 +642,8 @@ void initEditor()
   E.numrows = 0;
   E.row = NULL;
   E.filename = NULL;
+  E.statusmsg[0] = '\0';
+  E.statusmsg_time = 0;
   if (getWindowSize(&E.screenrows, &E.screencols) == -1)
     die("getWindowSize");
   E.screenrows -= 2;
@@ -606,6 +657,8 @@ int main(int argc, char *argv[])
   {
     editorOpen(argv[1]);
   }
+
+  editorSetStatusMessage("HELP: Ctrl-Q = quit");
 
   // char c;
 
