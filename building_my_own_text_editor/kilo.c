1e52539d6c06168c987c2bec0e1ac808cacb4b13
Selith <selithrubasingha@gmail.com>
2025-12-22 11:59:55 +0530

    removing enter ,backspace etc. from input for now

--- building_my_own_text_editor/kilo.c
+++ building_my_own_text_editor/kilo.c
@@ -25,6 +25,7 @@
 //the keys we use in the text editor
 enum editorKey
 {
+  BACKSPACE = 127,
   ARROW_LEFT = 1000,
   ARROW_RIGHT,
   ARROW_UP,
@@ -643,6 +644,9 @@ void editorProcessKeypress()
   int c = editorReadKey();
   switch (c)
   {
+  case '\r'://this is actually the ENTER key!
+      /* TODO */
+    break;
   case CTRL_KEY('q'):
     write(STDOUT_FILENO, "\x1b[2J", 4);
     write(STDOUT_FILENO, "\x1b[H", 3);
@@ -655,6 +659,12 @@ void editorProcessKeypress()
   //go to the end of a line (where the text ends not the screen)
     if (E.cy < E.numrows)
       E.cx = E.row[E.cy].size;
+    break;
+
+  case BACKSPACE:
+  case CTRL_KEY('h')://xtrl h is for backspace command
+  case DEL_KEY:
+    /* TODO */
     break;
 
   case PAGE_UP:
@@ -670,6 +680,10 @@ void editorProcessKeypress()
   case ARROW_LEFT:
   case ARROW_RIGHT:
     editorMoveCursor(c);
+    break;
+
+  case CTRL_KEY('l')://ctrl l is for refreshing the screen
+  case '\x1b':
     break;
   default:
     editorInsertChar(c);
